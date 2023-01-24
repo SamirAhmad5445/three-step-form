8cbed11b2e2e2af2fad3e063f29c093b454a112f
SamirAhmad5445 <samirahmad5445@gmail.com>
2023-01-24 22:29:33 +0200

    fixing some issues

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let buttons = document.querySelectorAll(".btn");
+let label = document.querySelector(".label");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -9,7 +10,11 @@ buttons.forEach((button) => {
       .querySelector(`.step-${button.getAttribute("data-index")}`)
       .classList.remove("active");
     if (button.getAttribute("data-index") == "3") {
-      location.reload();
+      let content = document.createElement("p");
+      content.innerHTML = "All Steps are Completed";
+      content.className = "complete-flag";
+      label.appendChild(content);
+      return;
     }
     document
       .querySelector(`.step-${button.getAttribute("data-index")} + *`)
